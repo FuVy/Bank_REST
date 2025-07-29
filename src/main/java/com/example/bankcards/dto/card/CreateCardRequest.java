8103e8c843fd876a15590e0e580cb6acd21ef786
Dzmitry <amigo1044@yandex.ru>
2025-07-29 19:36:34 +0300

    Update CreateCardRequest.java

--- src/main/java/com/example/bankcards/dto/card/CreateCardRequest.java
+++ src/main/java/com/example/bankcards/dto/card/CreateCardRequest.java
@@ -13,7 +13,7 @@ public class CreateCardRequest {
     @Pattern(regexp = "^\\d{16}$", message = "Card number must be 16 digits.")
     String cardNumber;
 
-    @NotBlank(message = "Owner ID can't be blank.")
+    @NotNull(message = "Owner ID can't be blank.")
     UUID ownerId;
 
     @NotNull(message = "Expiry date can't be null.")
